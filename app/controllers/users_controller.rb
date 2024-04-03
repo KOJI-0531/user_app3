7da2dddeea548bd05e1fb71a1091610ab5d9df6b
EC2 Default User <ec2-user@ip-172-31-10-75.ap-northeast-1.compute.internal>
2024-04-03 10:18:17 +0000

    error homework

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,25 @@ class UsersController < ApplicationController
     @users = User.all.order(created_at: :desc)
   end
   
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+  
+   def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.email = params[:email]
+    @user.save
+    redirect_to("/index")
+  end
+  
+ def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    redirect_to("/index")
+ end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/' => "users#new"
-  post 'users/create' => "users#create"
-  
   get 'index' => "users#index"
+  get 'users/:id/edit' => "users#edit"
+  get 'users/:id' => "users#show"
+  post 'users/create' => "users#create"
+  post 'users/:id/update' => "users#update"
+  post 'users/:id/destroy' => "users#destroy"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
